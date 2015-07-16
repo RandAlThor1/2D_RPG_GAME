6c5d0af9ce49c8a22008d4fbb015b4ce4812b17b
RandAlThor1 <benjaminallen1@hotmail.com>
2015-07-16 16:42:43 -0500

    player changes

--- src/pkg2drpg/Player.java
+++ src/pkg2drpg/Player.java
@@ -17,11 +17,45 @@ public class Player {
     public int Intelligence;
     public int Wisdom;
     public int Charisma;
+    
+    public int StrengthMod;
+    public int ConstitutionMod;
+    public int DexterityMod;
+    public int IntelligenceMod;
+    public int WisdomMod;
+    public int CharismaMod;
+    
     public int Health;
     public int Speed;
     public int DodgeChance;
+    
     public String CharacterName;
-     public static int Roll(int Amount, int Type){
+    
+    public static int[][] inventory = new int[4][9];
+    public static int[]   Hotbar    = new int   [9];
+    
+    public Player(int Strength, int Constitution, int Dexterity, int Intelligence, int Wisdom, int Charisma, String CharacterName){
+        this.Strength = Strength;
+        this.Constitution = Constitution;
+        this.Strength = Strength;
+        this.Dexterity = Dexterity;
+        this.Intelligence = Intelligence;
+        this.Wisdom = Wisdom;
+        
+        this.StrengthMod =      (int) ((Strength - 10) / 2);
+        this.ConstitutionMod =  (int) ((Constitution - 10) / 2);
+        this.DexterityMod =     (int) ((Dexterity - 10) / 2);
+        this.IntelligenceMod =  (int) ((Intelligence - 10) / 2);
+        this.WisdomMod =        (int) ((Wisdom - 10) / 2);
+        this.CharismaMod =      (int) ((Charisma - 10) / 2);
+        
+        this.Health = (int) 200 + ((Constitution - 10) / 2) * 50;
+        this.Speed = (int) 100 + ((Dexterity - 10) / 2) * 25;
+        this.DodgeChance = (int) 10 + ((Dexterity - 10) / 2) * 5;
+        
+        this.CharacterName = CharacterName;
+    }
+    public static int Roll(int Amount, int Type){
         int result = 0;
         for (int i = 0; i < Amount; i++) {
             result += (int)(Math.random() * Type) + 1;
